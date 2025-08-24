002fae72bb2357a866eeffd286eae96bd84133cb
anthonytobiesq <anthonytobiesq@gmail.com>
2025-08-24 11:39:21 +0300

    added write to file function : write_file.py, changed test.py

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 from functions.get_files_info import get_files_info
 from functions.get_file_content import get_file_content
+from functions.write_file import write_file
 
 '''if __name__ == "__main__":
     result = get_files_info("calculator", ".")
@@ -14,7 +15,7 @@ from functions.get_file_content import get_file_content
     result4 = get_files_info("calculator", "../")
     print("Result for '../' directory:\n    " + result4)'''
 
-if __name__ == "__main__":
+'''if __name__ == "__main__":
     result = get_file_content("calculator", "main.py")
     print(f"contents of file:\n" + result)
     print(" ")
@@ -25,4 +26,18 @@ if __name__ == "__main__":
     print(f"contents of file:\n" + result)
     print(" ")
     result = get_file_content("calculator", "pkg/does_not_exist.py")
-    print(f"contents of file:\n" + result)
+    print(f"contents of file:\n" + result)'''
+
+def test():
+    result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
+    print(result)
+    print(" ")
+    result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
+    print(result)
+    print(" ")
+    result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
+    print(result)
+
+
+if __name__ == "__main__":
+    test()
